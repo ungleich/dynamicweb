0f1fe738953d5108e0c0d06d04aef5bdc0648ea8
Nico Schottelius <nico@freiheit.schottelius.org>
2015-05-02 20:41:11 +0200

    can save messages
    Signed-off-by: Nico Schottelius <nico@freiheit.schottelius.org>

--- digital_glarus/views.py
+++ digital_glarus/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
+import datetime
 
 from django.shortcuts import get_object_or_404, render
-
-
+from django.forms import ModelForm
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from .models import Message
 
-
-def detail(request):
-    pass
 
 def detail(request, message_id):
     p = get_object_or_404(Message, pk=message_id)
@@ -19,7 +17,26 @@ def detail(request, message_id):
 def send_message(request):
     pass
 
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'email', 'phone_number', 'message' ]
+
 def index(request):
+    if request.method == 'POST':
+        message = Message(received_date=datetime.datetime.now())
+        form = MessageForm(request.POST, instance=message)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("digital_glarus:index"))
+
+    form = MessageForm()
     message_list = Message.objects.order_by('-received_date')[:5]
-    context = { 'message_list': message_list, }
+
+    context = { 
+        'message_list': message_list, 
+        'form': form,
+    }
+
     return render(request, 'digital_glarus/index.html', context)
