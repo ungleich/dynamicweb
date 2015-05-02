ca25436440a75c5cff6a364283e47bf78b9385bb
Nico Schottelius <nico@freiheit.schottelius.org>
2015-05-02 22:42:08 +0200

    make the form work
    Signed-off-by: Nico Schottelius <nico@freiheit.schottelius.org>

--- digital_glarus/urls.py
+++ digital_glarus/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'about$', views.about, name='about'),
     url(r'contact$', views.contact, name='contact'),
-    url(r'send_message', views.send_message, name='send_message'),
     url(r'^(?P<message_id>[0-9]+)/$', views.detail, name='detail'),
 ]
