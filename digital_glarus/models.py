1dff90c99b5ccc81563d5952fb0c1b64ae917df3
Nico Schottelius <nico@freiheit.schottelius.org>
2015-05-02 17:00:15 +0200

    start digital glarus
    Signed-off-by: Nico Schottelius <nico@freiheit.schottelius.org>

---
+++ digital_glarus/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Message(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=200)
+    message = models.TextField()
+
+    received_date = models.DateTimeField('date received')
