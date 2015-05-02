5fe7987b8adeb9ca0ac62e9687954c06c2589125
Nico Schottelius <nico@freiheit.schottelius.org>
2015-05-02 22:02:19 +0200

    integrate contact - begin to integrate form
    Signed-off-by: Nico Schottelius <nico@freiheit.schottelius.org>

--- digital_glarus/urls.py
+++ digital_glarus/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'about$', views.about, name='about'),
+    url(r'contact$', views.contact, name='contact'),
     url(r'send_message', views.send_message, name='send_message'),
     url(r'^(?P<message_id>[0-9]+)/$', views.detail, name='detail'),
 ]
