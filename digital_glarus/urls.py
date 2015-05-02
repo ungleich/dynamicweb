aab8845a085b33183771b61cfe70650427a5897a
Nico Schottelius <nico@freiheit.schottelius.org>
2015-05-02 22:48:14 +0200

    also integrate about
    Signed-off-by: Nico Schottelius <nico@freiheit.schottelius.org>

--- digital_glarus/urls.py
+++ digital_glarus/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'about$', views.about, name='about'),
     url(r'contact$', views.contact, name='contact'),
-    url(r'^(?P<message_id>[0-9]+)/$', views.detail, name='detail'),
 ]
