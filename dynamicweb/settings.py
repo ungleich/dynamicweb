cccd076a3b99c5125f10e98b1e78aba9d8e3b767
Nico Schottelius <nico@wurzel.schottelius.org>
2015-05-14 01:22:20 +0200

    update db to postgres
    Signed-off-by: Nico Schottelius <nico@wurzel.schottelius.org>

--- dynamicweb/settings.py
+++ dynamicweb/settings.py
@@ -79,11 +79,10 @@ WSGI_APPLICATION = 'dynamicweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'app',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
