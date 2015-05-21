7b8fe8723825e37d3f97cfc2066738bb9a911ce9
rscnt <rascnt@gmail.com>
2015-05-20 23:30:03 -0600

    urls: added django-cms urls to project urls.py
    Signed-off-by: rscnt <rascnt@gmail.com>

--- dynamicweb/urls.py
+++ dynamicweb/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'dynamicweb.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^digital.glarus/', include('digital_glarus.urls', namespace="digital_glarus")),
-]
+    url(r'^digital.glarus/', include('digital_glarus.urls',
+        namespace="digital_glarus")),
+    url(r'^', include('cms.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
