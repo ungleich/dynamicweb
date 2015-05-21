40ca3f37a09703543f48af39a57c4aaea1778093
rscnt <rascnt@gmail.com>
2015-05-21 01:21:22 -0600

    urls: imports, forgot, dumb.
    Signed-off-by: rscnt <rascnt@gmail.com>

--- dynamicweb/urls.py
+++ dynamicweb/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+# deprecated in version 1.8
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from dynamicweb import settings
 
 urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
