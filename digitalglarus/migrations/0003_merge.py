cd234c4dda4f35c4cf7d214a3ed1aeae6c1b9546
Nico Schottelius <nico@wurzel.schottelius.org>
2015-06-10 23:34:33 +0200

    digital_glarus -> digitalglarus
    Signed-off-by: Nico Schottelius <nico@wurzel.schottelius.org>

--- digitalglarus/migrations/0003_merge.py
+++ digitalglarus/migrations/0003_merge.py
@@ -7,8 +7,8 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('digital_glarus', '0002_auto_20150522_0450'),
-        ('digital_glarus', '0002_auto_20150527_1023'),
+        ('digitalglarus', '0002_auto_20150522_0450'),
+        ('digitalglarus', '0002_auto_20150527_1023'),
     ]
 
     operations = [
