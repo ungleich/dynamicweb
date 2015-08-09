589f0f60a7e8c6d96a999e963519177f38351c8a
상희 김 <verysanghee@ungleich.ch>
2015-08-09 22:13:38 +0200

    nodejs created

--- railshosting/urls.py
+++ railshosting/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'beta$', views.beta, name='beta'),
-    url(r'djangohosting$', views.djangohosting, name='djangohosting'),
+    url(r'djangohosting$', views.djangohosting, name='djangohosting'), 
+    url(r'nodejshosting$', views.nodejshosting, name='nodejshosting'),
+    url(r'railshosting$', views.railshosting, name='railshosting'),
 ]
