2c4f48a0cbf3559bc4159b8e81d9c6c06cf9a68f
Dominique Roux <dominique@roux.li>
2015-09-20 16:37:54 +0200

    Changed index page
    The index page is no set to the crowdfounding page.
    The old index page is now called home.
    The crowdfounding page is now in the menu on the first place,
    and the home page second.

--- digitalglarus/urls.py
+++ digitalglarus/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'about$', views.about, name='about'),
     url(r'contact$', views.contact, name='contact'),
-    url(r'landing$', views.landing, name='landing'),
+    url(r'letscowork$', views.letscowork, name='letscowork'),
+    url(r'home$', views.home, name='home'),
 ]
