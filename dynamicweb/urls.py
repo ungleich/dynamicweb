0a83b1494df9189ca448bbefa73ea2a895b8e30b
rscnt <rascnt@gmail.com>
2015-09-22 05:28:34 -0600

    dynamicweb: hotfix railshosting

--- dynamicweb/urls.py
+++ dynamicweb/urls.py
@@ -2,24 +2,21 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 # deprecated in version 1.8
 from django.conf.urls.static import static
-# i18n
-from django.conf.urls.i18n import i18n_patterns
 
 from dynamicweb import settings
+from hosting.views import railshosting
 
 urlpatterns = [
     url(r'^digitalglarus/', include('digitalglarus.urls',
-                                     namespace="digitalglarus")),
+                                    namespace="digitalglarus")),
     url(r'^hosting/', include('hosting.urls', namespace="hosting")),
-    url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
-    url(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# note the django CMS URLs included via i18n_patterns
-urlpatterns += i18n_patterns('',
+    url(r'^railshosting/', railshosting, name="rails.hosting"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
-)
+    url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
+    url(r'^jsi18n/(?P<packages>\S+?)/$',
+        'django.views.i18n.javascript_catalog'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += patterns('',
