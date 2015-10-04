9d38d3aee6303319fd0e07dc632f1d501599f6b7
rscnt <rascnt@gmail.com>
2015-10-04 09:38:01 -0600

    digitalglarus: i18n django-cms conf

--- dynamicweb/urls.py
+++ dynamicweb/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 # deprecated in version 1.8
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 
 from dynamicweb import settings
@@ -11,12 +12,16 @@ urlpatterns = [
                                     namespace="digitalglarus")),
     url(r'^hosting/', include('hosting.urls', namespace="hosting")),
     url(r'^railshosting/', railshosting, name="rails.hosting"),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('cms.urls')),
     url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
     url(r'^jsi18n/(?P<packages>\S+?)/$',
         'django.views.i18n.javascript_catalog'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# note the django CMS URLs included via i18n_patterns
+urlpatterns += i18n_patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+)
 
 if settings.DEBUG:
     urlpatterns += patterns('',
