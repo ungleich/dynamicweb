a4c3c341a30bdf57877fcf978a856926544a8267
rscnt <rascnt@gmail.com>
2015-10-04 16:48:33 -0600

    digitalglarus: changes on urls

--- digitalglarus/urls.py
+++ digitalglarus/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'_about$', views.about, name='about'),
-    url(r'_contact$', views.contact, name='contact'),
-    url(r'_letscowork$', views.letscowork, name='letscowork'),
-    url(r'_home$', views.home, name='home'),
+    url(r'old^$', views.index, name='index'),
+    url(r'old_about$', views.about, name='about'),
+    url(r'old_contact$', views.contact, name='contact'),
+    url(r'old_letscowork$', views.letscowork, name='letscowork'),
+    url(r'old_home$', views.home, name='home'),
     url(r'blog/$', views.blog, name='blog'),
     url(r'^blog/(?P<slug>\w[-\w]*)/$', views.blog_detail, name='blog-detail'),
 ]
