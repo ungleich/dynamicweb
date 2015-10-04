f9b3cfbf48e55a3d086b9fa4779577677b0a6ce3
rscnt <rascnt@gmail.com>
2015-10-04 10:44:25 -0600

    templates: cms, removed DG.Detail from django-cms
    Signed-off-by: rscnt <rascnt@gmail.com>

--- templates/cms/__init__.py
+++ templates/cms/__init__.py
@@ -5,7 +5,6 @@ TEMPLATES = {
     'digitalglarus/contact.html': _('DG.Contact'),
     'digitalglarus/index.html': _('DG.Home'),
     'digitalglarus/letscowork.html': _('DG.CoWork'),
-    'digitalglarus/detail.html': _('DG.Detail'),
     'ungleichch/blog.html': _('Blog'),
     'ungleichch/page.html': _('Page'),
 }
