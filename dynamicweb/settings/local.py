aec2cd429814375baf2ba7fbb2bd7aa8ef168bc9
rscnt <rascnt@gmail.com>
2015-11-02 14:44:18 -0600

    dynamicweb: added settings module with different files for each env
    Signed-off-by: rscnt <rascnt@gmail.com>

---
+++ dynamicweb/settings/local.py
@@ -0,0 +1,19 @@
+from .base import *
+
+ALLOWED_HOSTS = [
+    "*"
+    ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'app.db',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake'
+    }
+}
