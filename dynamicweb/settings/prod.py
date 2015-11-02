f885036d0ec7b86e8c37af969cf668dc1354f7b4
rscnt <rascnt@gmail.com>
2015-11-02 14:44:18 -0600

    dynamicweb: added settings module with different files for each env
    Signed-off-by: rscnt <rascnt@gmail.com>

---
+++ dynamicweb/settings/prod.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+    ('Nico Schottelius', 'nico.schottelius@ungleich.ch'),
+)
+#    ('Sanghee Kim', 'sanghee.kim@ungleich.ch'),
+
+MANAGERS = ADMINS
+
+ALLOWED_HOSTS = [
+    ".ungleich.ch",
+    "digital.glarus.ungleich.ch" ,
+]
