559fe2f698f0737c6466a98f6df6715b98d42957
rscnt <rascnt@gmail.com>
2016-02-03 07:27:29 -0600

    adding supporters view
    Signed-off-by: rscnt <rascnt@gmail.com>

--- digitalglarus/urls.py
+++ digitalglarus/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'old_contact$', views.contact, name='contact'),
     url(r'old_letscowork$', views.letscowork, name='letscowork'),
     url(r'old_home$', views.home, name='home'),
+    url(r'supporters/$', views.supporters, name='supporters'),
     url(r'blog/$', views.blog, name='blog'),
     url(r'^blog/(?P<slug>\w[-\w]*)/$', views.blog_detail, name='blog-detail'),
 ]
