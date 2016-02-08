ed08e54a55355661e67d3d41cd478189ab23e59f
rscnt <rascnt@gmail.com>
2016-02-08 02:43:08 -0600

    added digital glarus admin

--- digitalglarus/admin.py
+++ digitalglarus/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Message, Supporter
+from .models import Message, Supporter, DGGallery, DGPicture
 
+class DGPictureInline(admin.StackedInline):
+    model = DGPicture
+
+class DGGalleryAdmin(admin.ModelAdmin):
+    inlines = [DGPictureInline]
+
+admin.site.register(DGGallery, DGGalleryAdmin)
 admin.site.register(Message)
 admin.site.register(Supporter)
