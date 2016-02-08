b072df90fe117e33a55b77b3bba2ff7e6cdf2f50
rscnt <rascnt@gmail.com>
2016-02-08 04:31:53 -0600

    dg: supporters styles

--- digitalglarus/models.py
+++ digitalglarus/models.py
@@ -17,7 +17,7 @@ class Message(models.Model):
 
 class Supporter(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return "%s" % (self.name)
