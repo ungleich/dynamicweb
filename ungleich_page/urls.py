c5e5640e8d90b4b21244231c3cca905641ec6796
Levi <levinoelvm@gmail.com>
2016-04-12 00:50:15 -0500

    Added translation support for ungleich contact page,

--- ungleich_page/urls.py
+++ ungleich_page/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import ContactView
+from django.utils.translation import ugettext_lazy as _
+
 
 urlpatterns = [
-    url(r'contact/?$', ContactView.as_view(), name='contact'),
+    url(_(r'contact/?$'), ContactView.as_view(), name='contact'),
 ]
