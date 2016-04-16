043d5f28c3dce5a58370a97adfb243a7224939b4
Levi <levinoelvm@gmail.com>
2016-04-15 22:25:57 -0500

    fixing url translations

--- digitalglarus/urls.py
+++ digitalglarus/urls.py
@@ -5,7 +5,7 @@ from . import views
 from .views import ContactView, IndexView, AboutView
 
 urlpatterns = [
-    url(_(r'index/?$'), IndexView.as_view(), name='index'),
+    url(_(r'home/?$'), IndexView.as_view(), name='home'),
     url(_(r'about/?$'), AboutView.as_view(), name='about'),
     url(_(r'contact/?$'), ContactView.as_view(), name='contact'),
     url(_(r'supporters/?$'), views.supporters, name='supporters'),
