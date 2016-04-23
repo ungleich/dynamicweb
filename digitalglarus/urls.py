213f295394a18583653d77b6cb17835f1eb92768
Tomislav R <tmslav@gmail.com>
2016-04-23 19:00:20 +0200

    digitalglarus booking update

--- digitalglarus/urls.py
+++ digitalglarus/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(_(r'about/?$'), AboutView.as_view(), name='about'),
     url(_(r'contact/?$'), ContactView.as_view(), name='contact'),
     url(_(r'supporters/?$'), views.supporters, name='supporters'),
+    url(_(r'calendar_api/(?P<month>\d+)/(?P<year>\d+)?$'),views.CalendarApi.as_view()),
+    url(_(r'calendar_api/'),views.CalendarApi.as_view()),
     url(_(r'support-us/?$'), views.support, name='support'),
     url(r'^blog/(?P<slug>\w[-\w]*)/$', views.blog_detail, name='blog-detail'),
     url(r'blog/$', views.blog, name='blog'),
--- membership/urls.py
+++ membership/urls.py
@@ -5,9 +5,10 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = (
-    url(r"^$", views.LoginRegistrationView.as_view(), name='login'),
+    url(r"^$", views.LoginRegistrationView.as_view(), name='login_glarus'),
     url(r"^validate/(?P<validate_slug>.*)/$", views.validate_email),
     url(r"^membership/$", login_required(views.MembershipView.as_view()), name='membership'),
+    url(r'logout/?$',views.logout_glarus,name='logout_glarus'),
     url(r"^buy/(?P<time>\w+)/$", login_required(views.CreditCardView.as_view()), name='payment'),
     url(r'^buy/(?P<time>\w+)/reset',login_required(views.reset),name='reset')
 )
