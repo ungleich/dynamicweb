1486843af010d5a03b0fd6fa048887572b2bbadc
Levi <levinoelvm@gmail.com>
2016-05-04 00:16:41 -0500

    Added VM detail page, Added VM setting page, Added VM pricing page,Added VM orders page, Added VM status page

--- hosting/urls.py
+++ hosting/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 
 from .views import DjangoHostingView, RailsHostingView, PaymentVMView, \
     NodeJSHostingView, LoginView, SignupView, IndexView, \
-    OrdersHostingListView, OrdersHostingDetailView, VirtualMachinesPlanListView
+    OrdersHostingListView, OrdersHostingDetailView, VirtualMachinesPlanListView,\
+    VirtualMachineDetailListView
 
 urlpatterns = [
     url(r'index/?$', IndexView.as_view(), name='index'),
@@ -13,6 +14,8 @@ urlpatterns = [
     url(r'orders/?$', OrdersHostingListView.as_view(), name='orders'),
     url(r'orders/(?P<pk>\d+)/?$', OrdersHostingDetailView.as_view(), name='orders'),
     url(r'my-virtual-machines/?$', VirtualMachinesPlanListView.as_view(), name='virtual_machines'),
+    url(r'my-virtual-machines/(?P<pk>\d+)/?$', VirtualMachineDetailListView.as_view(),
+        name='virtual_machines'),
     url(r'login/?$', LoginView.as_view(), name='login'),
     url(r'signup/?$', SignupView.as_view(), name='signup'),
     url(r'^logout/?$', 'django.contrib.auth.views.logout',
