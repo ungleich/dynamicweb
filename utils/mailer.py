b85ecd5c38fe352c56a7e34b809eead98460061d
Levi <levinoelvm@gmail.com>
2016-06-24 00:49:46 -0500

    changing defualt 'from email' to no-reply@ungleich.ch

--- utils/mailer.py
+++ utils/mailer.py
@@ -21,7 +21,7 @@ class BaseEmail(object):
 
         self.email = EmailMultiAlternatives(self.subject, text_content)
         self.email.attach_alternative(html_content, "text/html")
-        self.email.from_email = 'no-replay@ungleich.ch'
+        self.email.from_email = '(Ungleich) Support <no-reply@ungleich.com>'
         self.email.to = [kwargs.get('to', 'info@ungleich.com')]
 
     def send(self):
