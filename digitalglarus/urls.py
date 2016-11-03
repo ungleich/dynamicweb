2102823f8e830eabf969c466f0ac50a41d86eaae
Levi <levinoelvm@gmail.com>
2016-11-03 12:59:32 -0500

    added supportus view

--- digitalglarus/urls.py
+++ digitalglarus/urls.py
@@ -7,11 +7,12 @@ from .views import ContactView, IndexView, AboutView, HistoryView, LoginView, Si
     MembershipPricingView, BookingSelectDatesView, BookingPaymentView, OrdersBookingDetailView,\
     BookingOrdersListView, MembershipOrdersListView, OrdersMembershipDetailView, \
     MembershipDeactivateView, MembershipDeactivateSuccessView, UserBillingAddressView, \
-    MembershipReactivateView
+    MembershipReactivateView, SupportusView
 # from membership.views import LoginRegistrationView
 
 urlpatterns = [
     url(_(r'^$'), IndexView.as_view(), name='landing'),
+    url(_(r'support-us/?$'), SupportusView.as_view(), name='supportus'),
     url(_(r'contact/?$'), ContactView.as_view(), name='contact'),
     url(_(r'login/?$'), LoginView.as_view(), name='login'),
     url(_(r'signup/?$'), SignupView.as_view(), name='signup'),
@@ -47,7 +48,7 @@ urlpatterns = [
     url(_(r'supporters/?$'), views.supporters, name='supporters'),
     url(r'calendar_api/(?P<month>\d+)/(?P<year>\d+)?$', views.CalendarApi.as_view(),name='calendar_api_1'),
     url(r'calendar_api/', views.CalendarApi.as_view(),name='calendar_api'),
-    url(_(r'support-us/?$'), views.support, name='support'),
+    # url(_(r'support-us/?$'), views.support, name='support'),
     url(r'^blog/(?P<slug>\w[-\w]*)/$', views.blog_detail, name='blog-detail'),
     url(r'blog/$', views.blog, name='blog'),
 ]
