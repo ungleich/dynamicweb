402a6d001ea3f6b7a0e87bb8658eb215ea08f950
unknown <nquintero@wbinnova.com>
2016-12-05 11:24:46 -0400

    spring 5 without the notice box

--- dynamicweb/settings/base.py
+++ dynamicweb/settings/base.py
@@ -181,8 +181,9 @@ CMS_TEMPLATES = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'app'
-
+        'NAME': 'app',
+		'USER': 'app',
+		'HOST': 'db'
     }
 }
 
