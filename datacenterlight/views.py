ea2f6444dbcf4bfe7909e5af9bfbb150379fdb68
Levi <levinoelvm@gmail.com>
2017-02-14 23:34:06 -0500

    Added alplora and datacenter beta program

--- datacenterlight/views.py
+++ datacenterlight/views.py
@@ -1,9 +1,75 @@
-from django.views.generic import FormView, CreateView
+from django.views.generic import FormView, CreateView, TemplateView
+from django.http import HttpResponseRedirect
 from .forms import BetaAccessForm
-from .models import BetaAccess
+from .models import BetaAccess, BetaAccessVMType, BetaAccessVM
 from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy, reverse
 from utils.mailer import BaseEmail
+
+
+class LandingProgramView(TemplateView):
+    template_name = "datacenterlight/landing.html"
+
+
+class BetaProgramView(CreateView):
+    template_name = "datacenterlight/beta.html"
+    model = BetaAccessVM
+    fields = '__all__'
+    # form_class = BetaAccessForm
+    # success_url = "/datacenterlight#requestform"
+    success_message = "Thank you, we will contact you as soon as possible"
+
+    def get_success_url(self):
+        success_url = reverse('datacenterlight:beta')
+        success_url += "#success"
+        return success_url
+
+    def get_context_data(self, **kwargs):
+        vms = BetaAccessVMType.objects.all()
+        context = super(BetaProgramView, self).get_context_data(**kwargs)
+        context.update({
+            'base_url': "{0}://{1}".format(self.request.scheme, self.request.get_host()),
+            'vms': vms
+        })
+        return context
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        BetaAccessVM.create(data)
+        return HttpResponseRedirect(self.get_success_url())
+
+    # def form_valid(self, form):
+
+    #     context = {
+    #         'base_url': "{0}://{1}".format(self.request.scheme, self.request.get_host())
+    #     }
+
+    #     email_data = {
+    #         'subject': 'DatacenterLight Beta Access Request',
+    #         'to': form.cleaned_data.get('email'),
+    #         'context': context,
+    #         'template_name': 'request_access_confirmation',
+    #         'template_path': 'datacenterlight/emails/'
+    #     }
+    #     email = BaseEmail(**email_data)
+    #     email.send()
+
+    #     context.update({
+    #         'email': form.cleaned_data.get('email')
+    #     })
+
+    #     email_data = {
+    #         'subject': 'DatacenterLight Beta Access Request',
+    #         'to': 'info@ungleich.ch',
+    #         'context': context,
+    #         'template_name': 'request_access_notification',
+    #         'template_path': 'datacenterlight/emails/'
+    #     }
+    #     email = BaseEmail(**email_data)
+    #     email.send()
+
+    #     messages.add_message(self.request, messages.SUCCESS, self.success_message)
+    #     return super(IndexView, self).form_valid(form)
 
 
 class IndexView(CreateView):
