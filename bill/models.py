e711a268b1bfaf40a987cf5bc82bbbee83179035
Modulos <modulos@protonmail.com>
2017-03-16 17:01:36 +0100

    Add bill model draft

--- bill/models.py
+++ bill/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from utils.models import BillingAddress
 
-# Create your models here.
+class Bill(models.Model):
+    customer = models.ForeignKey(Customer)
+    items = models.ForeignKey(Item)
+    date = model.DateField()
+    mwst_percent = model.FloatField
+    # TODO: We could calc these things on the fly
+    mwst = model.FloatField
+    netto = model.FloatField
+    brutto = model.FloatField
+
+class Customer(model.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    address = BillingAddress
+    rate = model.FloatField
+
+class Item(model.Model):
+    description = model.TextField
+    # Netto, brruto price is calced via Bill.mwst_percent
+    price = model.FloatField
+
