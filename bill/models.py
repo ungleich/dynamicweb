1cd45468bc17fbe87053f2cc3cf50915d7e089a0
Modulos <modulos@protonmail.com>
2017-03-21 11:37:04 +0700

    Fix typos

--- bill/models.py
+++ bill/models.py
@@ -1,23 +1,27 @@
 from django.db import models
 from utils.models import BillingAddress
+from membership.models import CustomUser
+
+
+class Customer(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    address = BillingAddress
+    rate = models.FloatField
+
+class Item(models.Model):
+    description = models.TextField
+    # Netto, brutto price is calced via Bill.mwst_percent
+    price = models.FloatField
+
 
 class Bill(models.Model):
     customer = models.ForeignKey(Customer)
     items = models.ForeignKey(Item)
-    date = model.DateField()
-    mwst_percent = model.FloatField
+    date = models.DateField()
+    mwst_percent = models.FloatField
     # TODO: We could calc these things on the fly
-    mwst = model.FloatField
-    netto = model.FloatField
-    brutto = model.FloatField
+    mwst = models.FloatField
+    netto = models.FloatField
+    brutto = models.FloatField
 
-class Customer(model.Model):
-    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    address = BillingAddress
-    rate = model.FloatField
-
-class Item(model.Model):
-    description = model.TextField
-    # Netto, brruto price is calced via Bill.mwst_percent
-    price = model.FloatField
 
