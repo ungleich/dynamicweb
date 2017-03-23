cb672a454364af068754e7b92d40081fda45ae96
Modulos <modulos@protonmail.com>
2017-03-23 17:09:49 +0700

    Correct items and bills realtionship

--- bill/models.py
+++ bill/models.py
@@ -6,29 +6,32 @@ from membership.models import CustomUser
 class Customer(models.Model):
     user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     address = BillingAddress
-    rate = models.FloatField
+    rate = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.user_id.email
 
+
+class Bill(models.Model):
+    customer = models.ForeignKey(Customer)
+    date = models.DateField()
+    mwst_percent = models.FloatField(default=1.08)
+    # TODO: We could calc these things on the fly
+    mwst = models.FloatField(default=0.0)
+    netto = models.FloatField(default=0.0)
+    brutto = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return "Bill for customer: {}".format(self.customer)
+
 class Item(models.Model):
-    description = models.TextField
+    description = models.TextField(default="")
     # Netto, brutto price is calced via Bill.mwst_percent
-    price = models.FloatField
+    price = models.FloatField(default=0.0)
+    # Many to Many relationship with bills 
+    bills = models.ManyToManyField(Bill)
 
     def __str__(self):
         return self.description
 
 
-class Bill(models.Model):
-    customer = models.ForeignKey(Customer)
-    items = models.ForeignKey(Item)
-    date = models.DateField()
-    mwst_percent = models.FloatField
-    # TODO: We could calc these things on the fly
-    mwst = models.FloatField
-    netto = models.FloatField
-    brutto = models.FloatField
-
-    def __str__(self):
-        return "Bill for customer: {}".format(self.customer)
