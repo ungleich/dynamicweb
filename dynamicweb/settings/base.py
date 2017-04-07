b774c34f12e5d463877d0abf8d81f351653a3921
Modulos <modulos@protonmail.com>
2017-04-07 06:27:32 +0200

    Add django-methfilters
    In order to calculate the correct netto price of an item based on the
    current mwst I decided to use the mathfilter in the template. Note that
    this could be also achived by using a "netto" field in the item itself.

--- dynamicweb/settings/base.py
+++ dynamicweb/settings/base.py
@@ -91,6 +91,7 @@ INSTALLED_APPS = (
     # 'djangocms_inherit',
     # 'djangocms_link',
     # 'djangocms_teaser',
+    'mathfilters',
     'djangocms_page_meta',
     'djangocms_text_ckeditor',
     'djangocms_admin_style',
