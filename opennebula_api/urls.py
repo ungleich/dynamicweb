4fae677ec65846f369f0f96a02df5a028ab70b50
modulos <modulos@protonmail.com>
2017-05-20 14:57:35 +0200

    Use cpu if vpcu is not available
    The template serializer needs ether vpcu or cpu to be present

--- opennebula_api/urls.py
+++ opennebula_api/urls.py
@@ -6,10 +6,6 @@ from .views import TemplateCreateView, TemplateDetailsView,\
 urlpatterns = {
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    url(r'^templates/$', TemplateCreateView.as_view(), name="template_create"),
-    url(r'^templates/(?P<pk>[0-9]+)/$', TemplateDetailsView.as_view(),
-        name="templates_details"),
-    
     url(r'^vms/$', VmCreateView.as_view(), name="vm_create"),
     url(r'^vms/(?P<pk>[0-9]+)/$', VmDetailsView.as_view(),
         name="vm_details"),
