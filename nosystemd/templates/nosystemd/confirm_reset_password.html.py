c205bb05e83f4e5b826166a03e29c30e629475cc
Henry Bravo <henry.bravo89@gmail.com>
2017-05-23 18:32:06 -0500

    Fixed Pricing pages bugs

---
+++ nosystemd/templates/nosystemd/confirm_reset_password.html.py
@@ -0,0 +1,50 @@
+BBBBBBB BBBBBBBBBBBBBBBBBBBBB
+BBBB BBBBBBBBBBB BBBBBBBBBB
+BBBB BBBB
+
+BBBBB BBBBBBB
+
+    XXXXXXXX
+        XXXX XXXXXXXXXXXXXXXXXXXXXXX
+            XXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                XXXX XXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXX
+
+                    XXXX XXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXX
+                        XXXX XXXXXXXXXXXXXXXXXX
+                            XXXX XXXXXXXXXXXXXXXXX
+                                XXXX XXXXXXXXXXXXXXXXXXXXXX
+                                    BB BBBBBBBB
+                                        XXX XXXXXXXXXXXXXXXXXXXXXX
+                                        BBB BBBBBBB BB BBBBBBBB
+                                            XXXXXXXXX
+                                        BBBBBB
+                                        XXXXX
+                                    BBBBB
+
+                                    XXX  XXXXXXXXXXXXXXXXXXXXXXXX gettext('Set your new password') XXXXX
+
+                                    XXXXX XXXXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXX XXXXXXXXXXX
+                                        BBBBBBBBBB
+                                        BBB BBBBB BB BBBB
+                                            BBBBBBBBBBBBBBB BBBBB BBBBBBBBBBBBBBBB
+                                        BBBBBB
+                                        BBBBBBB
+                                            XXXXXXX XXXXXXXXXXXXX XXXXXXXXXX XXXXXXXXXXXXX
+                                             gettext('Reset') 
+                                            XXXXXXXXX
+                                        BBBBBBBBBB
+                                    XXXXXXX
+                                    XXXXXX gettext('Already have an account ?') XX XXXXXXXXXXXXXX XXXXXXBBB BBBBBBBBBBBBBBBXX gettext('Log in') XXXXXXXXXXX
+                                    XXX XXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXX
+                                    XXXXX
+                              XXXXXX
+                            XXXXXX
+                        XXXXXX
+                    XXXXXX
+
+                XXXXXX
+            XXXXXX
+        XXXXXX
+    XXXXXXXXX
+
+BBBBBBBB
---
+++ nosystemd/templates/nosystemd/emails/password_reset_email.html.py
@@ -0,0 +1,13 @@
+BBBB BBBBBBBBBBBBBB BBB
+ gettext("You're receiving this email because you requested a password reset for your user account at %(site_name)s.") SSSSSS SSSSSSSSS SSSS SSSSS SSSSSSS SSS SSSSSSSSS S SSSSSSSS SSSSS SSS SSSS SSSS SSSSSSS SS SSSSSSSSSSSSSS
+
+ gettext('Please go to the following page and choose a new password:') 
+    BBBBB BBBBBBBBBB
+        BBB BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB BBBBBBBBBB BBBBBBBBBBB 
+    BBBBBBBB
+
+ gettext('Thanks for using our site!') 
+
+ gettext('The %(site_name)s team') SSS SSSSSSSSSSSSS SSSS
+
+BBBBBBBBBBBBB
