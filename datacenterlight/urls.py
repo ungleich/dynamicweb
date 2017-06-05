515cbff001617d2a98e0bab86f41ea9f37e1ee83
Henry Bravo <henry.bravo89@gmail.com>
2017-06-05 16:03:32 -0500

    copy PricingView

--- datacenterlight/urls.py
+++ datacenterlight/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import IndexView, BetaProgramView, LandingProgramView, BetaAccessView, PricingView, SuccessView
+from .views import IndexView, BetaProgramView, LandingProgramView, BetaAccessView, PricingView, SuccessView, OrderView
 
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^/beta-program/?$', BetaProgramView.as_view(), name='beta'),
     url(r'^/landing/?$', LandingProgramView.as_view(), name='landing'),
     url(r'^/pricing/?$', PricingView.as_view(), name='pricing'),
-    url(r'^/pricing-success/?$', SuccessView.as_view(), name='success'),
+    url(r'^/order/?$', OrderView.as_view(), name='order'),
+    url(r'^/order-success/?$', SuccessView.as_view(), name='success'),
     url(r'^/beta_access?$', BetaAccessView.as_view(), name='beta_access'),
 ]
