55836482934a78dc28f81ca76c08da87ec352a27
M.Ravi <mondi.ravi@gmail.com>
2017-06-06 01:43:43 +0530

    Changed the url for 'Order Now!' button on dcl's index page. It is now datacenterlight/order instead of datacenterlight/pricing

--- datacenterlight/urls.py
+++ datacenterlight/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^/?$', IndexView.as_view(), name='index'),
     url(r'^/beta-program/?$', BetaProgramView.as_view(), name='beta'),
     url(r'^/landing/?$', LandingProgramView.as_view(), name='landing'),
-    url(r'^/pricing/?$', PricingView.as_view(), name='pricing'),
+    url(r'^/order/?$', PricingView.as_view(), name='pricing'),
     url(r'^/beta_access?$', BetaAccessView.as_view(), name='beta_access'),
 ]
