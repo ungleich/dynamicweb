cd524350d8748e0f9c7f2232be9760e6f55116e2
M.Ravi <mondi.ravi@gmail.com>
2017-06-07 03:05:21 +0530

    Attempted manual merge to avoid conflicts

--- datacenterlight/views.py
+++ datacenterlight/views.py
@@ -7,12 +7,17 @@ from django.core.urlresolvers import reverse_lazy, reverse
 from utils.mailer import BaseEmail
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django import forms
+from django.core.exceptions import ValidationError
 
 from opennebula_api.models import OpenNebulaManager
 from opennebula_api.serializers import VirtualMachineTemplateSerializer
 
 class LandingProgramView(TemplateView):
     template_name = "datacenterlight/landing.html"
+
+class SuccessView(TemplateView):
+    template_name = "datacenterlight/success.html"
 
 class PricingView(TemplateView):
     template_name = "datacenterlight/pricing.html"
@@ -61,6 +66,81 @@ class PricingView(TemplateView):
         }
 
         return redirect(reverse('hosting:payment'))
+
+
+class OrderView(TemplateView):
+    template_name = "datacenterlight/order.html"
+
+    def get(self, request, *args, **kwargs):
+        try:
+            manager = OpenNebulaManager()
+            templates = manager.get_templates()
+
+            context = {
+                'templates': VirtualMachineTemplateSerializer(templates, many=True).data,
+            }
+        except:
+            messages.error( request,
+                'We have a temporary problem to connect to our backend. \
+                Please try again in a few minutes'
+                )
+            context = {
+                'error' : 'connection'
+                    }
+
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+
+        cores = request.POST.get('cpu')
+        memory = request.POST.get('ram')
+        storage = request.POST.get('storage')
+        price = request.POST.get('total')
+        template_id = int(request.POST.get('config'))
+        manager = OpenNebulaManager()
+        template = manager.get_template(template_id)
+        template_data = VirtualMachineTemplateSerializer(template).data
+        
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        name_field = forms.CharField()
+        email_field = forms.EmailField()
+        try:
+            name = name_field.clean(name)
+        except ValidationError as err:
+            messages.add_message(self.request, messages.ERROR, '%(value) is not a proper name.'.format(name))
+            return HttpResponseRedirect(reverse('datacenterlight:order'))
+
+        try:    
+            email = email_field.clean(email)
+        except ValidationError as err:
+            messages.add_message(self.request, messages.ERROR, '%(value) is not a proper email.'.format(email))
+            return HttpResponseRedirect(reverse('datacenterlight:order'))
+        
+        # We have valid email and name of the customer, hence send an 
+        # email to the admin
+        
+        context = {
+            'base_url': "{0}://{1}".format(self.request.scheme, self.request.get_host()),
+            'name': name,
+            'email': email,
+            'cores': cores,
+            'memory': memory,
+            'storage': storage,
+            'price': price,
+            'template': template_data['name'],
+        }
+        email_data = {
+            'subject': 'New Order Received',
+            'to': 'info@ungleich.ch',
+            'context': context,
+            'template_name': 'new_order_notification',
+            'template_path': 'datacenterlight/emails/'
+        }
+        email = BaseEmail(**email_data)
+        email.send()
+
+        return HttpResponseRedirect(reverse('datacenterlight:order_success'))
 
 
 class BetaAccessView(FormView):
