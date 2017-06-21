66f82572f1ce1a93e5dd35bf9724b1257e05d35a
M.Ravi <mondi.ravi@gmail.com>
2017-06-21 13:15:37 +0530

    Added payment url

--- datacenterlight/urls.py
+++ datacenterlight/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import IndexView, BetaProgramView, LandingProgramView, BetaAccessView, PricingView, SuccessView
+from .views import IndexView, BetaProgramView, LandingProgramView, BetaAccessView, PricingView, SuccessView, PaymentOrderView
 
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^/beta-program/?$', BetaProgramView.as_view(), name='beta'),
     url(r'^/landing/?$', LandingProgramView.as_view(), name='landing'),
     url(r'^/pricing/?$', PricingView.as_view(), name='pricing'),
+    url(r'^/payment/?$', PaymentOrderView.as_view(), name='payment'),
     url(r'^/order-success/?$', SuccessView.as_view(), name='order_success'),
     url(r'^/beta_access?$', BetaAccessView.as_view(), name='beta_access'),
 ]
