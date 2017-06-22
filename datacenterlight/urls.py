e848c318d0813a203c5d0d8bce9e35b96f07d1d6
M.Ravi <mondi.ravi@gmail.com>
2017-06-22 13:18:54 +0530

    Addd order confirmation url

--- datacenterlight/urls.py
+++ datacenterlight/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import IndexView, BetaProgramView, LandingProgramView, BetaAccessView, PricingView, SuccessView, PaymentOrderView
+from .views import IndexView, BetaProgramView, LandingProgramView, BetaAccessView, PricingView, SuccessView, PaymentOrderView, OrderConfirmationView
 
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^/landing/?$', LandingProgramView.as_view(), name='landing'),
     url(r'^/pricing/?$', PricingView.as_view(), name='pricing'),
     url(r'^/payment/?$', PaymentOrderView.as_view(), name='payment'),
+    url(r'^/order-confirmation/(?P<pk>\d+)/?$', OrderConfirmationView.as_view(), name='order_confirmation'),
     url(r'^/order-success/?$', SuccessView.as_view(), name='order_success'),
     url(r'^/beta_access?$', BetaAccessView.as_view(), name='beta_access'),
 ]
