d27eff94415ef937df9c7ecc1195938e888d36b4
Geetha Mondi <mondi.geetha@gmail.com>
2017-06-28 12:59:30 +0530

    Added function for dropdown menu

--- datacenterlight/static/datacenterlight/js/main.js
+++ datacenterlight/static/datacenterlight/js/main.js
@@ -53,6 +53,11 @@
     /* ---------------------------------------------
      Nav panel classic
      --------------------------------------------- */
+     $('ul.nav li.dropdown').hover(function() {
+		  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+		}, function() {
+		  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+		});
     
     
     function _initScroll(){
