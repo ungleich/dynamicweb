3313dab485979df7cc9f8fdf95e2a9a977dcdcf0
PCoder <purple.coder@yahoo.co.uk>
2017-06-29 13:05:06 +0530

    Added get_value_from_dict filter to obtain values from a dict in templates

--- datacenterlight/templatetags/custom_tags.py
+++ datacenterlight/templatetags/custom_tags.py
@@ -22,3 +22,13 @@ def change_lang(context, lang=None, *args, **kwargs):
 
 
     return "%s" % url
+
+@register.filter('get_value_from_dict')
+def get_value_from_dict(dict_data, key):
+    """
+    usage example {{ your_dict|get_value_from_dict:your_key }}
+    """
+    if key:
+        return dict_data.get(key)
+    else :
+        return None
