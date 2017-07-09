5826154b919db0e561981036244d0547f8003401
Andrii Marynets <andrii.marynets@gmail.com>
2017-07-09 17:55:58 +0300

    Add analytics on page from context_processor using app name

--- utils/context_processor.py
+++ utils/context_processor.py
@@ -6,8 +6,8 @@ def google_analytics(request):
     Use the variables returned in this function to
     render your Google Analytics tracking code template.
     """
-    host = request.get_host()
-    ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_IDS', False).get(host)
+    app_name = request.resolver_match.app_name
+    ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_IDS', False).get(app_name)
     if not settings.DEBUG and ga_prop_id:
         return {
             'GOOGLE_ANALYTICS_PROPERTY_ID': ga_prop_id
