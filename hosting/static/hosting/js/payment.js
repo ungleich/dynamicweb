467bb8f11442b0281d99a32c57f6efa2af2ce8e7
PCoder <purple.coder@yahoo.co.uk>
2017-07-17 01:17:12 +0530

    Fixed a bug for Enter your credit card number text not showing up in german

--- hosting/static/hosting/js/payment.js
+++ hosting/static/hosting/js/payment.js
@@ -116,8 +116,8 @@ $(document).ready(function () {
         };
 
         var enter_ccard_text = "Enter your credit card number";
-        if (typeof window.enter_ccard_text !== 'undefined') {
-            enter_ccard_text = window.enter_ccard_text
+        if (typeof window.enter_your_card_text !== 'undefined') {
+            enter_ccard_text = window.enter_your_card_text;
         }
         var cardNumberElement = elements.create('cardNumber', {
             style: credit_card_text_style,
