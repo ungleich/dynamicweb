8ab52c339855a79100ed762874b81108d8f4b58c
PCoder <purple.coder@yahoo.co.uk>
2017-08-19 06:41:29 +0530

    faq.html now referred as DCL.Faq

--- dynamicweb/settings/base.py
+++ dynamicweb/settings/base.py
@@ -213,6 +213,8 @@ CMS_TEMPLATES = (
     # ungleich
     ('blog_ungleich.html', gettext('Blog')),
     ('page.html', gettext('Page')),
+    # dcl
+    ('datacenterlight/faq.html', gettext('DCL.Faq')),
 )
 
 DATABASES = {
