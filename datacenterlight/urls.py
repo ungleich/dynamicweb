12bb95f68588964db48d38fa557697e4f1677f9b
M.Ravi <mondi.ravi@gmail.com>
2017-08-29 10:46:16 +0200

    Fixed a mistake

--- datacenterlight/urls.py
+++ datacenterlight/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^t$', IndexView.as_view(), name='index_t'),
     url(r'^g$', IndexView.as_view(), name='index_g'),
-    url(r'^f$', IndexView.as_view(), name='index_g'),
+    url(r'^f$', IndexView.as_view(), name='index_f'),
     url(r'^whydatacenterlight/?$', WhyDataCenterLightView.as_view(),
         name='whydatacenterlight'),
     url(r'^beta-program/?$', BetaProgramView.as_view(), name='beta'),
