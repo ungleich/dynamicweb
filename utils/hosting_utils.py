dc6fa5428e40572bcf2a879328d0cfd086f20f60
PCoder <purple.coder@yahoo.co.uk>
2017-09-05 03:29:12 +0530

    Corrected filtering public ssh keys condition

--- utils/hosting_utils.py
+++ utils/hosting_utils.py
@@ -7,5 +7,5 @@ def get_all_public_keys(customer):
     :param customer: The customer whose public keys are needed
     :return: A list of public keys
     """
-    return UserHostingKey.objects.filter(
-        user__site__customuser=customer).values_list("public_key", flat=True)
+    return UserHostingKey.objects.filter(user_id=customer.id).values_list(
+        "public_key", flat=True)
