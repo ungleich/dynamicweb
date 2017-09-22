057a2c77a6ed649027a2b19bb977db69f6f21418
Arvind Tiwari <tiwariav@gmail.com>
2017-09-23 01:57:40 +0530

    password reset email, user activation email redesigned

--- datacenterlight/urls.py
+++ datacenterlight/urls.py
@@ -11,7 +11,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^test/$', TemplateView.as_view(
-        template_name='datacenterlight/emails/welcome_user.html')),
+        template_name='hosting/emails/password_reset_email.html')),
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^t/$', IndexView.as_view(), name='index_t'),
     url(r'^g/$', IndexView.as_view(), name='index_g'),
