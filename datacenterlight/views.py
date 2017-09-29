5393ffa10ddcf3c7001050199faaf01e95e62cb3
M.Ravi <mondi.ravi@gmail.com>
2017-09-29 09:16:00 +0200

    Small code reorganizing

--- datacenterlight/views.py
+++ datacenterlight/views.py
@@ -517,8 +517,8 @@ class OrderConfirmationView(DetailView):
                 'response_object').stripe_plan_id}])
         stripe_subscription_obj = subscription_result.get('response_object')
         # Check if the subscription was approved and is active
-        if stripe_subscription_obj is None or \
-                        stripe_subscription_obj.status != 'active':
+        if (stripe_subscription_obj is None
+                or stripe_subscription_obj.status != 'active'):
             msg = subscription_result.get('error')
             messages.add_message(self.request, messages.ERROR, msg,
                                  extra_tags='failed_payment')
