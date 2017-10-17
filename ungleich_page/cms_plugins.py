8e76a1c6713b649f69f0541b8b72328d1812f9f1
M.Ravi <mondi.ravi@gmail.com>
2017-10-17 15:52:30 +0200

    Set image and title dynamically

--- ungleich_page/cms_plugins.py
+++ ungleich_page/cms_plugins.py
@@ -1,13 +1,23 @@
+from cms.models.pluginmodel import CMSPlugin
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from cms.models.pluginmodel import CMSPlugin
+
+from .models import UngelichPicture
 
 
 @plugin_pool.register_plugin
 class SectionWithImagePlugin(CMSPluginBase):
-    model = CMSPlugin
+    model = UngelichPicture
     render_template = "ungleich_page/glasfaser/section_with_image.html"
     cache = False
+
+    def render(self, context, instance, placeholder):
+        context.update({
+            'image': instance.image,
+            'object': instance,
+            'placeholder': placeholder
+        })
+        return context
 
 
 @plugin_pool.register_plugin
--- ungleich_page/models.py
+++ ungleich_page/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from cms.models.pluginmodel import CMSPlugin
+from django.db import models
+from filer.fields.image import FilerImageField
 
-# Create your models here.
+
+class UngelichPicture(CMSPlugin):
+    image = FilerImageField(
+        null=True,
+        blank=True,
+        related_name="image",
+        on_delete=models.SET_NULL
+    )
+    title = models.CharField(max_length=200)
