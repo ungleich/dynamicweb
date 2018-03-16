449683c72f8b8d6425a739a097ebfe91b6aca57f
Arvind Tiwari <tiwariav@gmail.com>
2018-03-16 23:50:33 +0530

    fix flake8 error

--- datacenterlight/cms_plugins.py
+++ datacenterlight/cms_plugins.py
@@ -1,6 +1,5 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from cms.models.pluginmodel import CMSPlugin
 
 from .cms_models import (
     DCLBannerItemPluginModel, DCLBannerListPluginModel, DCLContactPluginModel,
