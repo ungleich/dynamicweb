67344f3949741f3d6c733b4605e4490c3fec0a18
kapphire <ken.sapphire87@yahoo.com>
2018-03-30 17:24:12 +1300

    authentication redirect and email verification, form unit test

---
+++ hosting/tests/__init__.py
@@ -0,0 +1,5 @@
+from hosting.tests.test_forms import *
+from hosting.tests.test_models import *
+from hosting.tests.test_views import *
+from hosting.tests.tests impot *
+from hosting.tests.auth import *
