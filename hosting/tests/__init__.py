098e08f094b10170659a8202991cc2e73c3aa9cc
kapphire <ken.sapphire87@yahoo.com>
2018-04-06 06:26:52 +1200

    unit test for authentication is upgraded based on comment.

--- hosting/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from hosting.tests.test_forms import *
-from hosting.tests.test_models import *
-from hosting.tests.test_views import *
-from hosting.tests.tests impot *
-from hosting.tests.auth import *
