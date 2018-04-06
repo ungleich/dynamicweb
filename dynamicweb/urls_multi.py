24a9b8ab78eff5e414c791bc9c659714857ab99c
PCoder <purple.coder@yahoo.co.uk>
2018-04-07 00:13:24 +0200

    Redirect multi-tenant urls to /cms also

--- dynamicweb/urls_multi.py
+++ dynamicweb/urls_multi.py
@@ -3,10 +3,12 @@ from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.views import static as static_view
+from django.views.generic import RedirectView
 
 urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cms/', include('cms.urls')),
+    url(r'^$', RedirectView.as_view(url='/cms')),
 )
 
 urlpatterns += [
