d296ce72cbdc7be902f3504b325383d0ff0a9d86
PCoder <purple.coder@yahoo.co.uk>
2018-04-15 21:37:51 +0200

    Register VMPricing with admin site

--- datacenterlight/admin.py
+++ datacenterlight/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from cms.admin.placeholderadmin import PlaceholderAdminMixin
 from .cms_models import CMSIntegration
+from .models import VMPricing
 
 
 class CMSIntegrationAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
     list_display = ('name', 'domain')
 
-
 admin.site.register(CMSIntegration, CMSIntegrationAdmin)
+admin.site.register(VMPricing)
